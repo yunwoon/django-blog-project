814bf1bc93df5f55841334bac66a7f633fb97cea
yunwoon <chea2089@naver.com>
2019-07-19 02:11:32 +0900

    make account

---
+++ project/blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.board,name="board"),
+    path('detail/<int:blog_id>',views.detail,name="detail"),
+    path('new/',views.new,name="new"),
+    path('create',views.create,name="create"),
+    #path('어떤 url이 들어오면',(어디에 있는)어떤함수를 실행시켜라)
+]
--- project/yunlog/urls.py
+++ project/yunlog/urls.py
@@ -14,21 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings #media 파일을 쓰기 위함 111
 from django.conf.urls.static import static #media 파일을 쓰기 위함 222
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blog.views.index,name="index"),
-    path('board/',blog.views.board,name="board"),
-    path('board/detail/<int:blog_id>',blog.views.detail,name="detail"),
-    path('board/new/',blog.views.new,name="new"),
-    path('board/create',blog.views.create,name="create"),
-    #path('어떤 url이 들어오면',(어디에 있는)어떤함수를 실행시켜라)
+    path('board/',include('blog.urls')),
     path('portfolio/',portfolio.views.portfolio,name="portfolio"),
+    path('accounts/',include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #MEDIA 를 더하는거임
 
 # 바로 위에 static 한거랑 같게 따로 쓰고 싶으면
