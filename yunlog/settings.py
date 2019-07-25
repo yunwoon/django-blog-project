f23b2bfe76f0f2c7f9699ba8305e7aa663c97ec3
yunwoon <chea2089@naver.com>
2019-07-25 10:31:52 +0900

    retry heroku -4

--- yunlog/settings.py
+++ yunlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9^cu%$29nc3gp+!bab^u2jyiw#a9)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
